dabde04e5c34207a89862c1978f600a342ea00c1
Thomas <pianist.chopin.1@gmail.com>
2021-01-23 23:55:38 +0000

    create Custom_clearmagentoshoppingcarts

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author TSN-Media Team
+ * @copyright Copyright (c) 2018 TSN-Media (https://tsn-media.com)
+ * @package SvgMap
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Custom_clearmagentoshoppingcarts',
+    __DIR__
+);
